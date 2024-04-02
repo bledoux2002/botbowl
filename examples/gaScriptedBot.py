64d666ab8c4da065d784e4504d66ff659a1cb13c
Benjamin Ledoux <bledoux2002@gmail.com>
2024-04-02 17:26:25 -0400

    added timer for entire sim

--- examples/gaScriptedBot.py
+++ examples/gaScriptedBot.py
@@ -918,6 +918,7 @@ def main():
     bestOverall = ["", -math.inf]
 
     plotFitness = [0]
+    totalTime = 0
 
     # Update first chromosome to test
     with open('data.json', 'r', encoding='utf-8') as chromoFile:
@@ -974,6 +975,7 @@ def main():
                 start = time.time()
                 game.init()
                 end = time.time()
+                totalTime += end
                 print(f"Time to complete: {end - start} seconds")
 
                 wins += 1 if game.get_winning_team() is game.state.home_team else 0
@@ -1029,6 +1031,7 @@ def main():
 
     output = f"{population_eval[0][0]} was the strongest candidate over {generation} generations, with a fitness score of {population_eval[0][1]}.\n"
     output += f"{bestOverall[0]} was the best candidate detected throughout the generations, with a fitness score of {bestOverall[1]}.\n"
+    output += f"Total time to execute: {totalTime}\n"
     print(output)
 
     now = datetime.now().strftime("%d-%m-%Y_%H.%M.%S")
